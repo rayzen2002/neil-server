f69fa8798fefa8c1e7f03ac52abe3a2af84115e1
Emanuel <emanunelzsmj@gmail.com>
2023-06-04 22:33:03 -0300

    Mudando porta default

--- server/src/server.js
+++ server/src/server.js
@@ -4,7 +4,7 @@ import cors from '@fastify/cors'
 import { authRoutes } from './routes/auth.js'
 import jwt from '@fastify/jwt'
 import { Routes } from './routes/routes.js'
-
+const port = process.env.PORT || 5000
 const app = fastify()
 app.register(Routes)
 app.register(authRoutes)
@@ -14,7 +14,4 @@ app.register(cors, {
 app.register(jwt, {
   secret: 'zolomonnajkndjksdnkajd',
 })
-app.listen({
-  port: 3333,
-  host: '0.0.0.0',
-})
+app.listen(port, () => console.log(`Server is  running on port ${port}`))
